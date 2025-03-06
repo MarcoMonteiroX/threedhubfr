e973eea1320b1ad8cf188539daa9058de36dfcdd
Marco <hehemarcos300@gmail.com>
2025-03-05 21:29:43 -0400

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://threedhub-production.up.railway.app',
+    baseURL: 'threedhub-production-2db9.up.railway.app',
     headers: { 'Content-Type': 'application/json' },
 });
 
